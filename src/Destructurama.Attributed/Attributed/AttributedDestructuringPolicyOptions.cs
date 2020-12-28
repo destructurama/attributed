31358c5ac8ad7159917bc8b9fa83ca4e85411de4
avtc <tarasenkov@gmail.com>
2020-12-28 18:13:40 +0200

    Update AttributedDestructuringPolicyOptions.cs

--- src/Destructurama.Attributed/Attributed/AttributedDestructuringPolicyOptions.cs
+++ src/Destructurama.Attributed/Attributed/AttributedDestructuringPolicyOptions.cs
@@ -3,7 +3,7 @@
     public class AttributedDestructuringPolicyOptions
     {
         /// <summary>
-        /// By setting IgnoreNullProperties to true no need to set [NotLoggedIfDefault] for every logged property.
+        /// By setting IgnoreNullProperties to true no need to set [NotLoggedIfNull] for every logged property.
         /// Custom types implemenenting IEnumerable, will be destructed as StructureValue and affected by IgnoreNullProperties 
         /// only in case at least one property (or the type itself) has Destructurama attribute applied.
         /// </summary>
